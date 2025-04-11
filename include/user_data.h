cade45016d4d9cbaf2140ef8af36f1597287ae5c
loren <ndunguloren96@gmail.com>
2025-04-11 14:56:23 +0300

    add: implement user data structure and function declarations

---
+++ include/user_data.h
@@ -0,0 +1,17 @@
+#ifndef USER_DATA_H
+#define USER_DATA_H
+
+#define MAX_LENGTH 100
+
+// Structure to store user data
+typedef struct {
+    char registration_number[MAX_LENGTH];
+    char password[MAX_LENGTH];
+} User;
+
+// Function declarations related to user data operations
+void save_user(User user);
+int login_user();
+void register_user();
+
+#endif // USER_DATA_H
