f06d5b7eca417264c64a3334b816b9801f240de8
loren <ndunguloren96@gmail.com>
2025-04-12 23:47:21 +0300

    add: implement auth_functions.h for authentication operations

---
+++ include/auth_functions.h
@@ -0,0 +1,11 @@
+#ifndef AUTH_FUNCTIONS_H
+#define AUTH_FUNCTIONS_H
+
+#include "auth_system.h"
+
+// Function declarations related to function operations for authentication
+void clear_input_buffer();
+void handle_registration();
+void handle_login();
+
+#endif // AUTH_FUNCTIONS_H
