dd17b09a4064fe269fa759c15c33cddd3ab397b0
loren <ndunguloren96@gmail.com>
2025-04-19 23:14:55 +0300

    feat: add functions to view all students and attendance reports

--- include/student_report.h
+++ include/student_report.h
@@ -4,4 +4,8 @@
 // Function to generate a report for a student
 void generate_student_report(const char* registration_number);
 
+// Admin functions
+void view_all_students();
+void view_all_attendance();
+
 #endif
