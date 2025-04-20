405fa592ff8aafaff3b70dc67a0dfcb7199eb00e
loren <ndunguloren96@gmail.com>
2025-04-20 17:44:10 +0300

    update: modify user management functions to include role parameter and enhance user struct

--- include/user_management.h
+++ include/user_management.h
@@ -3,9 +3,20 @@
 
 #define MAX_LENGTH 100
 
-// Function declarations for user management
+typedef enum { ROLE_STUDENT, ROLE_INSTRUCTOR, ROLE_ADMIN } UserRole;
+
+typedef struct {
+    int id;
+    char registration_number[MAX_LENGTH];
+    char first_name[MAX_LENGTH];
+    char last_name[MAX_LENGTH];
+    char email[MAX_LENGTH];
+    char role[MAX_LENGTH];
+} User;
+
 void register_user();
-int login_user(char *first_name, char *last_name);
+int login_user(char *first_name, char *last_name, char *role);
 void reset_password();
+void list_users(const char *role_filter); // List users by role
 
 #endif // USER_MANAGEMENT_H
