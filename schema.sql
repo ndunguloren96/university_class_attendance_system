3ecb9c53318ece7b5c9039f370e503f20ad6b727
loren <ndunguloren96@gmail.com>
2025-04-20 17:35:24 +0300

    fix: add first_name and last_name fields to users table for improved user data management

--- schema.sql
+++ schema.sql
@@ -1,7 +1,9 @@
 CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     registration_number TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL
 );
 
 CREATE TABLE attendance (
